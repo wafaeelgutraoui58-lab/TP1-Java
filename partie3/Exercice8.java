e3e0188ab77272132f02dace5617fc06e72b2096
wafaeelgutraoui58-lab <wafae.elgutraoui.58@edu.uiz.ac.ma>
2026-03-25 17:07:21 +0100

    Create Exercice8.java

---
+++ partie3/Exercice8.java
@@ -0,0 +1,29 @@
+package partie3;
+
+import java.util.Scanner;
+
+public class Exercice8 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[][] mat = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("Element [" + i + "][" + j + "] : ");
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
