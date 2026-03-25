dc4d8c5c48f4dd482f00ef2036973695d27af380
wafaeelgutraoui58-lab <wafae.elgutraoui.58@edu.uiz.ac.ma>
2026-03-25 17:04:59 +0100

    Create Exercice6.java

---
+++ partie2/Exercice6.java
@@ -0,0 +1,25 @@
+package partie2;
+
+import java.util.Scanner;
+
+public class Exercice6 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] tab = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Element " + i + " : ");
+            tab[i] = sc.nextInt();
+        }
+
+        System.out.print("Nombre a chercher: ");
+        int x = sc.nextInt();
+
+        for (int i = 0; i < 10; i++) {
+            if (tab[i] == x) {
+                System.out.println("Position: " + i);
+            }
+        }
